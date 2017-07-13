a0f6cbc19c632d9c41a48466241d38040bb26cf5
Cyrille Gindreau <cyrille@visgence.com>
2017-07-13 16:35:12 -0600

    added datetime picker

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ This will clear out your dist folder, build the project and start the server.
 
 npm run lint
 runs htmllint, csslint, eslint, and pep8 (configs haven't been completed yet.)
+
+
+Future updates:
+https://github.com/tempusdominus/bootstrap-3 for timepicker (in alpha)
+bootstrap 4 (in alpha)
+jquery 3 (bootstrap needs update)
--- src/js/timeController.js
+++ src/js/timeController.js
@@ -1,8 +1,25 @@
-export default class timeController {
-    constructor($http) {
-        'ngInject';
-        this._$http = $http;
-        console.log('hello time')
-    }
+import 'eonasdan-bootstrap-datetimepicker';
+// Hack until tempusdominus comes out of alpha, see readme for link.
+import './../css/bootstrap-datetimepicker.min.css';
 
+export default class timeController {
+    constructor($scope, $location) {
+        'ngInject';
+        this.$scope = $scope;
+        this.$location = $location;
+
+        console.log('hello time');
+
+        $(function () {
+            $('#startTimePicker').datetimepicker();
+            $('#endTimePicker').datetimepicker();
+        });
+
+        $scope.SubmitDates = () => {
+            const startTime = $('#startTimePicker').data('DateTimePicker').date();
+            const endTime = $('#endTimePicker').data('DateTimePicker').date();
+
+            console.log(startTime.unix(), endTime.unix());
+        };
+    }
 }
