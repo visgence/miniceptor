a03c70456a955d33029f960356629d288e1a88c0
Cyrille Gindreau <cyrille@visgence.com>
2017-08-02 13:35:35 -0600

    updating dockerfiles to be able to handle all edge cases

--- docker/app/run.sh
+++ docker/app/run.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
 MINICEPTOR=$(cd $(dirname "${BASH_SOURCE[0]}")/../../ && pwd -P)
 docker run -t -i -P \
+    --rm=true \
+    --name miniceptor \
     -v $MINICEPTOR:/home/miniceptor \
     -p 8000:8000 \
     miniceptor/app \
