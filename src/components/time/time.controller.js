e54968b1984911b6081d54c3892530836589f25a
Cyrille Gindreau <cyrille@visgence.com>
2017-08-09 10:52:53 -0600

    added history support

--- src/components/time/time.controller.js
+++ src/components/time/time.controller.js
@@ -9,21 +9,26 @@ export default class timeController {
 
     $onInit() {
 
-        this.$scope.startDate = new Date(this.$location.search().start);
-        this.$scope.startDate = new Date(this.$location.search().end);
+        if (this.$location.search().start !== undefined) {
+            this.$scope.startDate = new Date(this.$location.search().start * 1000);
+        }
+        if (this.$location.search().end !== undefined) {
+            this.$scope.endDate = new Date(this.$location.search().end * 1000);
+        }
 
         this.$scope.SubmitDates = () => {
             let startTime = this.$scope.startDate;
             let endTime = this.$scope.endDate;
+
             if (startTime !== undefined) {
                 startTime = new Date(startTime);
                 this.$location.search('start', startTime.getTime() / 1000);
             } else {
                 this.$location.search('start', null);
             }
-            console.log(endTime)
+
             if (endTime !== undefined) {
-                endTime = new Date(endTime)
+                endTime = new Date(endTime);
                 this.$location.search('end', endTime.getTime() / 1000);
             } else {
                 this.$location.search('end', null);
