18786e1b116bdf2ef313e4a7b016e54427d6a7ba
Jacob Bakarich <jacob@Michaels-Mini.dynamic.visgence.com>
2017-08-10 10:36:02 -0600

    Finished material redesign of Generate JSON page

--- src/components/generateJson/generateJson.controller.js
+++ src/components/generateJson/generateJson.controller.js
@@ -9,17 +9,20 @@ export default class generateJsonController {
     $onInit() {
         this.$scope.inputItems = [];
         this.$scope.outputItems = [];
-        this.$scope.addInput = () => {
+        this.$scope.escape = false;
 
+        //  intialize input object
+        this.$scope.addInput = () => {
             this.$scope.inputItems.push({
-                name: 'name',
-                type: 'type',
-                units: 'units',
-                description: 'description',
+                name: '',
+                type: '',
+                units: '',
+                description: '',
             });
 
         };
 
+        //  pop top object on remove button press
         this.$scope.removeInput = () => {
 
             this.$scope.inputItems.pop();
@@ -32,51 +35,59 @@ export default class generateJsonController {
 
         };
 
-
+        //  intialize output objects
         this.$scope.addOutput = () => {
             this.$scope.outputItems.push({
-                name: 'Name',
-                model: 'Model',
-                type: 'Type',
-                units: 'Units',
-                description: 'Description',
-                time_stamp: 'Time Stamp',
+                name: '',
+                model: '',
+                type: '',
+                units: '',
+                description: '',
+                time_stamp: '',
+                scale_cof: '',
             });
         };
 
+        //  on submit button press
         this.$scope.submit = () => {
             const jsonData = {};
 
             jsonData.uuid = this.$scope.uuid;
-            jsonData.model = $('#model').val();
-            jsonData.description = $('#description').val();
+            jsonData.model = this.$scope.model;
+            jsonData.description = this.$scope.description;
 
             const inputs = [];
             const outputs = [];
-            $('.sensor-input').each((index) => {
+
+            //  loops to pull correct items out of input/output object arrays.
+            for (let i = 0; i < this.$scope.inputItems.length; i++) {
                 const input = {};
-                input.name = $('input[name="name"]').val();
-                input.description = $('input[name="description"]').val();
-                input.sensor_type = $('input[name="sensor_type"]').val();
-                input.units = $('input[name="units"]').val();
+                input.name = this.$scope.inputItems[i].name;
+                input.description = this.$scope.inputItems[i].description;
+                input.sensor_type = this.$scope.inputItems[i].sensor_type;
+                input.units = this.$scope.inputItems[i].units;
                 inputs.push(input);
-            });
+            }
 
-            $('.sensor-output').each((index) => {
+            for (let i = 0; i < this.$scope.outputItems.length; i++) {
                 const output = {};
-                output.name = $('input[name="name"]').val();
-                output.model = $('input[name="model"]').val();
-                output.description = $('input[name="description"]').val();
-                output.sensor_type = $('input[name="sensor_type"]').val();
-                output.units = $('input[name="units"]').val();
-                output.timestamp = Number($('input[name="timestamp"]').val());
-
-                const scale = [];
-                scale.push(Number($('input[name="scale1"]').val()));
-                scale.push(Number($('input[name="scale2"]').val()));
-                output.scale = scale;
+                output.name = this.$scope.outputItems[i].name;
+                output.model = this.$scope.outputItems[i].model;
+                output.description = this.$scope.outputItems[i].description;
+                output.sensor_type = this.$scope.outputItems[i].type;
+                output.units = this.$scope.outputItems[i].units;
+                output.timestamp = this.$scope.outputItems[i].time_stamp;
                 outputs.push(output);
-            });
+            }
+
+            // $('.sensor-output').each((index) => {
+            //
+            //     const scale = [];
+            //     scale.push(Number($('input[name="scale1"]').val()));
+            //     scale.push(Number($('input[name="scale2"]').val()));
+            //     output.scale = scale;
+            //     outputs.push(output);
+            // });
 
             jsonData.out = outputs;
             jsonData.in = inputs;
@@ -84,10 +95,12 @@ export default class generateJsonController {
             let json = JSON.stringify(jsonData);
             const jsonLength = json.length;
 
-            if ($('#escape').is(':checked')) {
+            //  check if escape has been checked
+            if (this.$scope.escape) {
                 json = this.escape(json);
             }
 
+            // use material alert dialog to present finished JSON
             this.$mdDialog.show(
                 this.$mdDialog.alert()
                     .parent(angular.element(document.querySelector('#popupContainer')))
@@ -105,92 +118,4 @@ export default class generateJsonController {
         return text.replace(/"/g, '\\"');
     }
 
-    createInput(label, name) {
-        const input = $('<input/>', {
-            name: name,
-            type: 'text',
-            placeholder: name,
-            class: 'form-control input-md',
-        });
-
-        label = $('<label/>', {
-            class: 'col-md-5 control-label',
-            // 'for': id
-        }).html(label);
-
-        const group = $('<div/>', {
-            class: 'form-group',
-        });
-
-        const col = $('<div/>', {
-            class: 'col-md-4',
-        });
-
-        group.append(label);
-        col.append(input);
-        group.append(col);
-
-        return group;
-    }
-
-    createButton(desc, name, toRemove) {
-        const button = $('<button/>', {
-            // id: uuid,
-            name: name,
-            class: 'btn btn-danger',
-        }).html(desc);
-
-        const label = $('<label/>', {
-            class: 'col-md-5 control-label',
-            // 'for': id
-        }).html(desc);
-
-        const group = $('<div/>', {
-            class: 'form-group',
-        });
-
-        const col = $('<div/>', {
-            class: 'col-md-4',
-        });
-        button.click(() => {
-            console.log('button click');
-            toRemove.remove();
-            return false;
-        });
-        group.append(label);
-        col.append(button);
-        group.append(col);
-        return group;
-    }
-
-    createSensorInput() {
-        const sensor = $('<div/>', {
-            class: 'sensor-input',
-            style: 'padding-bottom: 30px',
-        });
-        sensor.append(this.createInput('Name', 'name'));
-        sensor.append(this.createInput('Type', 'sensor_type'));
-        sensor.append(this.createInput('Units', 'units'));
-        sensor.append(this.createInput('Description', 'description'));
-        sensor.append(this.createButton('Remove', 'remove', sensor));
-        console.log(sensor);
-        return sensor;
-    }
-
-    createSensorOutput() {
-        const sensor = $('<div/>', {
-            class: 'sensor-output',
-            style: 'padding-bottom: 30px',
-        });
-        sensor.append(this.createInput('Name', 'name'));
-        sensor.append(this.createInput('Type', 'sensor_type'));
-        sensor.append(this.createInput('Units', 'units'));
-        sensor.append(this.createInput('model', 'model'));
-        sensor.append(this.createInput('Description', 'description'));
-        sensor.append(this.createInput('Timestamp', 'timestamp'));
-        sensor.append(this.createInput('Scale Cof 1', 'scale1'));
-        sensor.append(this.createInput('Scale Cof 2', 'scale2'));
-        sensor.append(this.createButton('Remove', 'remove', sensor));
-        return sensor;
-    }
 }
